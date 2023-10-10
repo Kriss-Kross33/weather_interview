557432e2da960d7bc938c6faf683cddffe143f44
Kriss-Kross33 <kwamefosu33@gmail.com>
2023-10-10 00:48:25 +0000

    throw `ServerException` when response status code is not 200

--- lib/src/core/client/api_client.dart
+++ lib/src/core/client/api_client.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' show Client;
+import 'package:weather_interview/src/core/core.dart';
 import 'package:weather_interview/src/features/home/domain/entities/weather.dart';
 
 import '../../features/home/data/models/weather_model.dart';
@@ -10,12 +11,20 @@ class ApiClient {
   final Client _client;
 
   Future<Weather> fetchWeather(String city) async {
-    final url =
-        'https://api.weatherbit.io/v2.0/forecast/daily?city=$city&key=e33c406c16424c29b8e4f15e7575895f';
-    final uri = Uri.parse(url);
-    final response = await _client.get(uri, headers: {});
-    final responseJson = response.body;
-    final responseBody = jsonDecode(responseJson);
-    return WeatherModel.fromJson(responseBody);
+    try {
+      final url =
+          'https://api.weatherbit.io/v2.0/forecast/daily?city=$city&key=e33c406c16424c29b8e4f15e7575895f';
+      final uri = Uri.parse(url);
+      final response = await _client.get(uri, headers: {});
+      if (response.statusCode != 200) {
+        throw ServerException(
+            errorMessage: response.reasonPhrase ?? 'Unkwon Error');
+      }
+      final responseJson = response.body;
+      final responseBody = jsonDecode(responseJson);
+      return WeatherModel.fromJson(responseBody);
+    } catch (e) {
+      throw ServerException(errorMessage: e.toString());
+    }
   }
 }
